30f7d5b4d7bab1a068c43e540cd85873bbde0e11
macqueenism <a_macqueen@hotmail.com>
2014-10-03 11:54:43 -0700

    add player class

--- nfl_api.rb
+++ nfl_api.rb
@@ -5,24 +5,34 @@ require 'net/http'
 require 'rexml/document'
 
 # set global variable (both functions need to use it)
-@players = []
+PLAYERS = []
+
+# create players class
+class Player
+
+  def initialize(firstname, lastname, rank, team, pos)
+    @stats = {'firstname'=>firstname, 'lastname'=>lastname, 'rank'=>rank,
+      'team'=>team, 'pos'=>pos}
+  end
+
+  def all_stats(s)
+    s = @stats
+    puts "#{s['firstname']} #{s['lastname']}, #{s['team']} #{s['pos']}, rank: #{s['rank']}" 
+  end
+
+end
 
 # function that calls the nfl api and returns results in array of hashes 
 def nfl_api_caller(url)
   # get the XML data as a string
   xml_data = Net::HTTP.get_response(URI.parse(url)).body
-
   # extract event information
   doc = REXML::Document.new(xml_data)
 
   doc.elements.each('players/player') do |ele|
-    firstName = ele.attributes["firstName"]
-    lastName = ele.attributes["lastName"]
-    rank = ele.attributes["rank"]
-    team = ele.attributes["teamAbbr"]
-    position = ele.attributes["position"]
-    player = {"firstname" => firstName, "lastname" => lastName, "rank" => rank, "team" => team, "position" => position}
-    @players.push(player)
+    new_player = Player.new(ele.attributes["firstName"], ele.attributes["lastName"], 
+      ele.attributes["rank"], ele.attributes["teamAbbr"], ele.attributes["position"])
+    PLAYERS.push(new_player)
   end
 
 end
@@ -30,7 +40,7 @@ end
 def nfl_api_looper(number_of_records)
 
   # set api string and parameter names
-  url = 'http://api.fantasy.nfl.com/players/userdraftranks'
+  url = 'http://api.fantasy.nfl.com/players/editordraftranks'
 
   if number_of_records <=100
     # call api and get one load of xml data
@@ -46,13 +56,13 @@ def nfl_api_looper(number_of_records)
     end
 
     # only return the amount of records that was asked for
-    for i in number_of_records..@players.length do
-      @players.delete_at(i)
+    for i in number_of_records..PLAYERS.length do
+      PLAYERS.delete_at(i)
     end
   end
 end
 
 # puts the output nicely
-nfl_api_looper(250)
-@players.each {|i| puts "#{i['firstname']} #{i['lastname']}, #{i['team']} #{i['position']}, rank:#{i['rank']}\n"}
-puts @players.length
+nfl_api_looper(275)
+PLAYERS.each {|i| i.all_stats "\n"}
+puts PLAYERS.length
